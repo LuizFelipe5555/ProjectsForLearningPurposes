337a5e30090df52d1f468a777535c01abc66f77d
LuizDPT <luizfelipedpt@gmail.com>
2022-05-05 23:34:42 -0300

    questão 12 do beecrowd

---
+++ URI/1012/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split();
+
+            double a = double.Parse(valores[0]);
+            double b = double.Parse(valores[1]);
+            double c = double.Parse(valores[2]);
+
+            double triangulo = (a * c) / 2;
+            double circulo = (3.14159 * Math.Pow(c, 2));
+            double trapezio = ((a + b) * c) / 2;
+            double quadrado = Math.Pow(b, 2);
+            double retangulo = (a * b);
+
+            Console.WriteLine($"TRIANGULO: {triangulo:0.000}");
+            Console.WriteLine($"CIRCULO: {circulo:0.000}");
+            Console.WriteLine($"TRAPEZIO: {trapezio:0.000}");
+            Console.WriteLine($"QUADRADO: {quadrado:0.000}");
+            Console.WriteLine($"RETANGULO: {retangulo:0.000}");
+        }
+    }
+}
